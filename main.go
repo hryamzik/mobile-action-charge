99cdce4df438d7629219b834ade7485e8edd52a9
Roman Belyakovsky <ihryamzik@gmail.com>
2017-03-06 00:08:54 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+// import (
+//   "github.com/mikepb/go-serial"
+//   "log"
+// )
+//
+// func main() {
+//   options := serial.RawOptions
+//   options.BitRate = 9600
+//   p, err := options.Open("/dev/tty.usbserial")
+//   if err != nil {
+//     log.Panic(err)
+//   }
+//
+//   defer p.Close()
+//
+//   chargeBytes := []byte{0x55, 0x55, 0x55, 0x55, 0x4, 0x1}
+//
+//   buf := make([]byte, 1)
+//   if c, err := p.Read(buf); err != nil {
+//     log.Panic(err)
+//   } else {
+//     log.Println(buf)
+//     log.Println(c)
+//   }
+//   if c, err := p.Write(chargeBytes); err != nil {
+//     log.Panic(err)
+//   } else {
+//     log.Println(c)
+//   }
+//
+// }
+
+import (
+	"fmt"
+	"github.com/jacobsa/go-serial/serial"
+	"log"
+)
+
+func main() {
+	// Set up options.
+	options := serial.OpenOptions{
+		PortName:        "/dev/tty.usbserial",
+		BaudRate:        9600,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 4,
+	}
+
+	// Open the port.
+	port, err := serial.Open(options)
+	if err != nil {
+		log.Fatalf("serial.Open: %v", err)
+	}
+
+	// Make sure to close it later.
+	defer port.Close()
+
+	// Write 4 bytes to the port.
+	chargeBytes := []byte{0x55, 0x55, 0x55, 0x55, 0x4, 0x1}
+	n, err := port.Write(chargeBytes)
+	if err != nil {
+		log.Fatalf("port.Write: %v", err)
+	}
+
+	fmt.Println("Wrote", n, "bytes.")
+}
